31c118b542b71421ab0dd16c9b93650e857cabf9
m-lange <martin_lange_@gmx.net>
2020-05-13 18:34:37 +0200

    fix non-ref parameter

--- src/lib.rs
+++ src/lib.rs
@@ -42,3 +42,11 @@ impl fmt::Display for MultipleTransclusionError {
         write!(f, "Multiple transclusions of {:?}", self.0)
     }
 }
+
+#[cfg(test)]
+mod test {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
