61b2418c33bb111a7e35e88f54b02f02e7307c2d
Martin Lange <martin_lange_@gmx.net>
2021-03-08 12:31:58 +0100

    use different threads for events from sources and code, use timeout to aggregate events

--- src/watch.rs
+++ src/watch.rs
@@ -1,31 +1,39 @@
 use crate::{cmd, util::Fallible};
 
 use clap::ArgMatches;
-use notify::Watcher;
-use std::{
-    collections::HashSet, env, path::PathBuf, sync::mpsc::channel, thread::sleep, time::Duration,
-};
+use notify::{DebouncedEvent, Watcher};
+use std::time::Instant;
+use std::{collections::HashSet, env, path::PathBuf, sync::mpsc::channel, time::Duration};
+
+const COLLECT_EVENTS_MILLIS: u64 = 500;
+
+#[derive(PartialEq)]
+pub enum ChangeType {
+    Sources,
+    Code,
+}
 
 pub fn watch(
     args: ArgMatches,
     watch_sources: HashSet<PathBuf>,
     watch_code: HashSet<PathBuf>,
 ) -> Fallible {
-    trigger_on_change(&watch_sources, &watch_code, |paths| {
-        let is_forward = paths
-            .first()
-            .map(|p| watch_sources.contains(p))
-            .unwrap_or(true);
-
+    trigger_on_change(&watch_sources, &watch_code, |change| {
         println!(
             "{} changed. Re-building...",
-            if is_forward { "Sources" } else { "Code" }
+            if change == ChangeType::Sources {
+                "Sources"
+            } else {
+                "Code"
+            }
         );
 
-        let curr_dir = env::current_dir()?;
-        let (_config, _watch_forward, _watch_reverse) =
-            cmd::run_with_args(&args, Some(!is_forward))?;
-        env::set_current_dir(&curr_dir)?;
+        if change == ChangeType::Sources {
+            let curr_dir = env::current_dir()?;
+            let (_config, _watch_forward, _watch_reverse) =
+                cmd::run_with_args(&args, Some(change == ChangeType::Code))?;
+            env::set_current_dir(&curr_dir)?;
+        }
 
         Ok(())
     })?;
@@ -40,14 +48,14 @@ pub fn trigger_on_change<F>(
     closure: F,
 ) -> Fallible
 where
-    F: Fn(Vec<PathBuf>) -> Fallible,
+    F: Fn(ChangeType) -> Fallible,
 {
-    use notify::DebouncedEvent::*;
     use notify::RecursiveMode::*;
 
-    let (tx, rx) = channel();
-    let mut source_watcher = notify::watcher(tx.clone(), Duration::from_secs(1))?;
-    let mut code_watcher = notify::watcher(tx, Duration::from_secs(1))?;
+    let (tx_sources, rx_sources) = channel();
+    let mut source_watcher = notify::watcher(tx_sources, Duration::from_secs(1))?;
+    let (tx_code, rx_code) = channel();
+    let mut code_watcher = notify::watcher(tx_code, Duration::from_secs(1))?;
 
     for path in watch_sources {
         source_watcher.watch(path, NonRecursive)?;
@@ -56,22 +64,60 @@ where
         code_watcher.watch(path, NonRecursive)?;
     }
 
-    loop {
-        let first_event = rx.recv().unwrap();
-        sleep(Duration::from_millis(50));
-        let other_events = rx.try_iter();
+    let (tx_changes_sources, rx_changes) = channel();
+    let tx_changes_code = tx_changes_sources.clone();
+    std::thread::spawn(move || loop {
+        let deadline = Instant::now() + Duration::from_millis(COLLECT_EVENTS_MILLIS);
+        let mut send_event = false;
+        loop {
+            let timeout = deadline.saturating_duration_since(Instant::now());
+            if timeout.as_nanos() == 0 {
+                break;
+            }
 
-        let all_events = std::iter::once(first_event).chain(other_events);
-
-        let paths = all_events
-            .filter_map(|event| match event {
-                Create(path) | Write(path) | Remove(path) | Rename(_, path) => Some(path),
-                _ => None,
-            })
-            .collect::<Vec<_>>();
-
-        if !paths.is_empty() {
-            closure(paths)?;
+            if let Ok(event) = rx_sources.recv_timeout(timeout) {
+                if is_file_change(event) {
+                    send_event = true;
+                }
+            }
         }
+        if send_event {
+            tx_changes_sources.send(ChangeType::Sources).unwrap();
+        }
+    });
+
+    std::thread::spawn(move || loop {
+        let deadline = Instant::now() + Duration::from_millis(COLLECT_EVENTS_MILLIS);
+        let mut send_event = false;
+        loop {
+            let timeout = deadline.saturating_duration_since(Instant::now());
+            if timeout.as_nanos() == 0 {
+                break;
+            }
+
+            if let Ok(event) = rx_code.recv_timeout(timeout) {
+                if is_file_change(event) {
+                    send_event = true;
+                }
+            }
+        }
+        if send_event {
+            tx_changes_code.send(ChangeType::Code).unwrap();
+        }
+    });
+
+    loop {
+        let event = rx_changes.recv().unwrap();
+        closure(event)?;
     }
+}
+
+fn is_file_change(event: DebouncedEvent) -> bool {
+    matches!(
+        event,
+        DebouncedEvent::Create(_)
+            | DebouncedEvent::Write(_)
+            | DebouncedEvent::Remove(_)
+            | DebouncedEvent::Rename(_, _)
+    )
 }
