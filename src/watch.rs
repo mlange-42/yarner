6a9c43b531693e3de6115e92fd7424c099e61b14
Martin Lange <martin_lange_@gmx.net>
2021-03-08 15:22:15 +0100

    use for loop in receiver instead of closure

--- src/watch.rs
+++ src/watch.rs
@@ -1,7 +1,7 @@
 use crate::{cmd, util::Fallible};
 
 use clap::ArgMatches;
-use notify::{DebouncedEvent, Watcher};
+use notify::{DebouncedEvent, RecommendedWatcher, Watcher};
 use std::sync::mpsc::{Receiver, Sender};
 use std::time::Instant;
 use std::{env, path::PathBuf, sync::mpsc::channel, time::Duration};
@@ -19,7 +19,9 @@ where
     I: Iterator<Item = PathBuf>,
     J: Iterator<Item = PathBuf>,
 {
-    trigger_on_change(&mut watch_sources, &mut watch_code, |change| {
+    let rx_changes = trigger_on_change(&mut watch_sources, &mut watch_code)?;
+
+    for change in rx_changes {
         println!(
             "{} changed. Re-building...",
             if change == ChangeType::Sources {
@@ -35,21 +37,17 @@ where
                 cmd::run_with_args(&args, Some(change == ChangeType::Code))?;
             env::set_current_dir(&curr_dir)?;
         }
-
-        Ok(())
-    })?;
+    }
 
     Ok(())
 }
 
 /// Calls the closure when a book source file is changed, blocking indefinitely.
-fn trigger_on_change<F, I, J>(
+fn trigger_on_change<I, J>(
     watch_sources: &mut I,
     watch_code: &mut J,
-    closure: F,
 ) -> Fallible<Receiver<ChangeType>>
 where
-    F: Fn(ChangeType) -> Fallible,
     I: Iterator<Item = PathBuf>,
     J: Iterator<Item = PathBuf>,
 {
@@ -69,44 +67,50 @@ where
 
     let (tx_changes, rx_changes) = channel();
 
-    start_event_thread(rx_sources, tx_changes.clone(), ChangeType::Sources);
-    start_event_thread(rx_code, tx_changes, ChangeType::Code);
+    start_event_thread(
+        source_watcher,
+        rx_sources,
+        tx_changes.clone(),
+        ChangeType::Sources,
+    );
+    start_event_thread(code_watcher, rx_code, tx_changes, ChangeType::Code);
 
-    loop {
-        let event = rx_changes.recv().unwrap();
-        closure(event)?;
-    }
+    Ok(rx_changes)
 }
 
 fn start_event_thread(
+    watcher: RecommendedWatcher,
     in_channel: Receiver<DebouncedEvent>,
     out_channel: Sender<ChangeType>,
     event_type: ChangeType,
 ) {
-    std::thread::spawn(move || loop {
-        let mut send_event = false;
-
-        let event = in_channel.recv().unwrap();
-        if is_file_change(event) {
-            send_event = true;
-        }
-
-        let deadline = Instant::now() + Duration::from_millis(COLLECT_EVENTS_MILLIS);
+    std::thread::spawn(move || {
+        let _watcher = watcher;
         loop {
-            let timeout = match deadline.checked_duration_since(Instant::now()) {
-                None => break,
-                Some(timeout) => timeout,
-            };
+            let mut send_event = false;
 
-            if let Ok(event) = in_channel.recv_timeout(timeout) {
-                if is_file_change(event) {
-                    send_event = true;
+            let event = in_channel.recv().unwrap();
+            if is_file_change(event) {
+                send_event = true;
+            }
+
+            let deadline = Instant::now() + Duration::from_millis(COLLECT_EVENTS_MILLIS);
+            loop {
+                let timeout = match deadline.checked_duration_since(Instant::now()) {
+                    None => break,
+                    Some(timeout) => timeout,
+                };
+
+                if let Ok(event) = in_channel.recv_timeout(timeout) {
+                    if is_file_change(event) {
+                        send_event = true;
+                    }
                 }
             }
-        }
 
-        if send_event {
-            out_channel.send(event_type.clone()).unwrap();
+            if send_event {
+                out_channel.send(event_type.clone()).unwrap();
+            }
         }
     });
 }
